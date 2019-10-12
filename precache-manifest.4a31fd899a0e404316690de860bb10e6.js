bdf8ef4687394d396118c95d32003a60ffb04a17
Ketan <ketantoo@gmail.com>
2019-10-12 11:15:01 +0530

    Updates

---
+++ precache-manifest.4a31fd899a0e404316690de860bb10e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869ae6e87ea6e56c29585d8f21d66adc",
+    "url": "/Zero-To-Mastery-React-/monster-rolodex/index.html"
+  },
+  {
+    "revision": "fb3fb1f91b1419c0bb9b",
+    "url": "/Zero-To-Mastery-React-/monster-rolodex/static/css/main.f7bbf283.chunk.css"
+  },
+  {
+    "revision": "56022eb4a5dc281792e1",
+    "url": "/Zero-To-Mastery-React-/monster-rolodex/static/js/2.66e5ab65.chunk.js"
+  },
+  {
+    "revision": "fb3fb1f91b1419c0bb9b",
+    "url": "/Zero-To-Mastery-React-/monster-rolodex/static/js/main.0580a697.chunk.js"
+  },
+  {
+    "revision": "8449419d4173cd85d5af",
+    "url": "/Zero-To-Mastery-React-/monster-rolodex/static/js/runtime-main.c6b8eb79.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Zero-To-Mastery-React-/monster-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
