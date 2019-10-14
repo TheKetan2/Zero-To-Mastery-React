eb580f8457cf2995e607db1f6f989d7804c99881
Ketan <ketantoo@gmail.com>
2019-10-14 16:14:07 +0530

    started online shop project

---
+++ online-clothing/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./homepage_components";
+import HomePage from "./homepage_components";
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
