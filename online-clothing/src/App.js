8b221be402a4383195f6f1bd2f647cef86ed6031
Ketan <ketantoo@gmail.com>
2019-10-18 16:03:02 +0530

    created header component and styled it too

--- online-clothing/src/App.js
+++ online-clothing/src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header-component/header-component";
+
 const HatsPage = () => (
   <div>
     <h1>Hatas</h1>
@@ -17,6 +19,7 @@ const JacketsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
