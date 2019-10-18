ff3a549d8619bc620f19b5b16e9506fb2385072a
Ketan <ketantoo@gmail.com>
2019-10-19 02:29:07 +0530

    added google firebase authentication module

--- online-clothing/src/App.js
+++ online-clothing/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header-component/header-component";
-
+import SignInSignUpPages from "./pages/sign-in-sign-up-pages/sign-in-sign-up-pages";
 const HatsPage = () => (
   <div>
     <h1>Hatas</h1>
@@ -23,6 +23,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInSignUpPages} />
       </Switch>
     </div>
   );
---
+++ online-clothing/src/pages/sign-in-sign-up-pages/sign-in-sign-up-pages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./sign-in-sign-up.style.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInSignUpPages = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn></SignIn>
+  </div>
+);
+
+export default SignInSignUpPages;
